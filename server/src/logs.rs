7e6c056f1728e93bc8dd0d8ad00b5b72579855a0
pchchv <ipchchv@gmail.com>
2025-10-31 20:15:12 +0300

    feat(logs): change the time format

--- server/src/logs.rs
+++ server/src/logs.rs
@@ -1,7 +1,15 @@
-use tracing_subscriber::EnvFilter;
+use time::macros::format_description;
+use tracing_subscriber::{
+    EnvFilter,
+    fmt::{self, time::LocalTime},
+};
 
 pub fn init_logs() {
     let _filter = EnvFilter::default()
         .add_directive("server=trace".parse().unwrap())
         .add_directive("axum=info".parse().unwrap());
+
+    let _timer = LocalTime::new(format_description!(
+        "[day]-[month]-[year] [hour]:[minute]:[second]"
+    ));
 }
