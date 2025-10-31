7a3eaac25d5361e686f9c75e3e9a2f510c2b43b9
pchchv <ipchchv@gmail.com>
2025-10-31 20:26:44 +0300

    feat(logs): implement logging to a file and add log rotation

--- server/src/logs.rs
+++ server/src/logs.rs
@@ -6,7 +6,7 @@ use tracing_subscriber::{
     util::SubscriberInitExt,
 };
 
-pub fn init_logs() {
+pub fn init_logs() -> tracing_appender::non_blocking::WorkerGuard {
     let filter = EnvFilter::default()
         .add_directive("server=trace".parse().unwrap())
         .add_directive("axum=info".parse().unwrap());
@@ -14,6 +14,19 @@ pub fn init_logs() {
     let timer = LocalTime::new(format_description!(
         "[day]-[month]-[year] [hour]:[minute]:[second]"
     ));
-    
-    tracing_subscriber::registry().with(filter).with(fmt::layer().with_timer(timer)).init();
+
+    let exe_path = std::env::current_exe().expect("failed to get executable path");
+    let exe_folder = exe_path.parent().expect("failed to get executable folder");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(tracing_appender::rolling::daily(
+        exe_folder.join("logs"),
+        "server",
+    ));
+
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(fmt::layer().with_timer(timer))
+        .with(fmt::layer().with_writer(non_blocking).with_ansi(false))
+        .init();
+
+    _guard
 }
