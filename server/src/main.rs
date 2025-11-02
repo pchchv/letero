e5d66b72d07d5406fe74ef21dbbc2c3abf9c511f
pchchv <ipchchv@gmail.com>
2025-11-02 11:47:57 +0300

    feat: initialize TcpListener

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,16 @@
+use std::net::Ipv4Addr;
 use axum::{Router, routing::get};
 use server::init_logs;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let _guard = init_logs();
-
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let port = 4000;
+    let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, port))
+        .await
+        .expect("failed to bind port");
+
+    tracing::info!("Listening on port {port}");
 }
