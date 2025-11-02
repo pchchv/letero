13f6f0087a31bb11ccb3597ff6f91732c406b010
pchchv <ipchchv@gmail.com>
2025-11-02 11:45:21 +0300

    feat: add router

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use server::init_logs;
+
+#[tokio::main]
+async fn main() {
+    let _guard = init_logs();
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 }
