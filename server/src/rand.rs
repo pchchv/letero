66d255abf44a3f00d843527f3562c1e88cd79c60
pchchv <ipchchv@gmail.com>
2025-11-04 02:20:54 +0300

    feat(rand): implement a RandomGenerator

--- server/src/rand.rs
+++ server/src/rand.rs
@@ -1,9 +1,22 @@
-use rand::{SeedableRng, rngs::SmallRng};
+use rand::{Rng, SeedableRng, rngs::SmallRng};
 
 pub struct SmallRandom(SmallRng);
 
 impl SmallRandom {
     pub fn new(seed: u64) -> Self {
         SmallRandom(SmallRng::seed_from_u64(seed))
+    }
+}
+
+#[cfg_attr(test, mockall::automock)]
+pub trait RandomGenerator: Sync + Send {
+    fn get_salt(&mut self) -> String;
+}
+
+impl RandomGenerator for SmallRandom {
+    fn get_salt(&mut self) -> String {
+        let mut result = [0u8; 16];
+        self.0.fill(&mut result);
+        hex::encode(result)
     }
 }
