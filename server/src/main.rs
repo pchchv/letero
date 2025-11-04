0b52755b6e03a205d44154d27ace568fb49623f3
pchchv <ipchchv@gmail.com>
2025-11-04 11:50:33 +0300

    feat(state): add random

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,27 @@
 use std::{net::Ipv4Addr, sync::Arc};
-use axum::{Router, middleware, routing::get};
-use server::{init_logs, init_db, services::trace::trace};
-use tokio::net::TcpListener;
+use axum::{Router, middleware, routing::{get, post}};
+use server::{
+    init_db, init_logs,
+    AppState,
+    rand::SmallRandom,
+    services::{session, trace::trace},
+    controllers::users::{login_user, new_user},
+};
+use tokio::{net::TcpListener, sync::Mutex};
 
 #[tokio::main]
 async fn main() {
     let _guard = init_logs();
     let db = init_db().await;
-    let users = Arc::new(PgUsersRepository::new(db));
-    let sessions = Arc::new(PgSessionsRepository::new(db.clone()));
-    let state = Arc::new(AppState { users, sessions });
+    session::start_cleanup_task(db.clone());
+    let rng = Arc::new(Mutex::new(SmallRandom::new(807234275934919497)));
+    let state = Arc::new(AppState::new(rng, db));
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
+        .route("/users", post(new_user))
+        .route("/login", post(login_user))
         .with_state(state)
-        .layer(middleware::from_fn(trace));
+        .route_layer(middleware::from_fn(trace));
 
     let port = 4000;
     let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, port))
--- server/src/state.rs
+++ server/src/state.rs
@@ -1,7 +1,25 @@
-use crate::repositories::{sessions::SessionsRepository, users::UsersRepository};
+use crate::{
+    rand::RandomGenerator,
+    repositories::{
+        sessions::{SessionsRepository, PgSessionsRepository},
+        users::{UsersRepository, PgUsersRepository},
+    },
+};
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 pub struct AppState {
+    pub random: Arc<Mutex<dyn RandomGenerator>>,
     pub users: Arc<dyn UsersRepository>,
     pub sessions: Arc<dyn SessionsRepository>,
+}
+
+impl AppState {
+    pub fn new(random: Arc<Mutex<dyn RandomGenerator>>, pool: sqlx::PgPool) -> Self {
+        Self {
+            users: Arc::new(PgUsersRepository::new(pool.clone())),
+            sessions: Arc::new(PgSessionsRepository::new(pool)),
+            random,
+        }
+    }
 }
