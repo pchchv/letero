8aef2ff5833e9b597415ee26237d5133ccb8f6cb
pchchv <ipchchv@gmail.com>
2025-11-07 20:42:01 +0300

    feat(repositories): implement create_chat and remove_chat functions

--- server/src/repositories/chats.rs
+++ server/src/repositories/chats.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::{PgPool, query};
 use std::collections::HashSet;
 use crate::{
     error::RepositoryError,
@@ -29,5 +29,44 @@ pub struct PgChatsRepository(PgPool);
 impl PgChatsRepository {
     pub fn new(pool: PgPool) -> Self {
         Self(pool)
+    }
+}
+
+#[async_trait::async_trait]
+impl ChatsRepository for PgChatsRepository {
+    async fn create_chat(
+        &self,
+        title: &ChatTitle,
+        users: &[UserId],
+    ) -> Result<ChatId, RepositoryError> {
+        let mut tn = self.0.begin().await?;
+
+        let chat_id = ChatId::new(
+            sqlx::query_scalar!(
+                "INSERT INTO Chats (Title) VALUES ($1) RETURNING Id",
+                title as _
+            )
+            .fetch_one(&mut *tn)
+            .await?,
+        );
+
+        query!(
+            "INSERT INTO ChatMembers (ChatId, UserId) SELECT $1, UNNEST($2::int[])",
+            chat_id as _,
+            users as _,
+        )
+        .execute(&mut *tn)
+        .await?;
+
+        tn.commit().await?;
+
+        Ok(chat_id)
+    }
+
+    async fn remove_chat(&self, chat_id: ChatId) -> Result<(), RepositoryError> {
+        sqlx::query!("DELETE FROM Chats WHERE Id = $1", chat_id as _)
+            .execute(&self.0)
+            .await?;
+        Ok(())
     }
 }
