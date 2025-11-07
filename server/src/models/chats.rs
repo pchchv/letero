ebf8f41a234a89a2fd76e196706da04fe6468ae5
pchchv <ipchchv@gmail.com>
2025-11-07 20:56:22 +0300

    feat(models): implement ChatId

--- server/src/models/chats.rs
+++ server/src/models/chats.rs
@@ -33,3 +33,39 @@ impl Deref for ChatTitle {
         &self.0
     }
 }
+
+#[derive(Debug, sqlx::Type, Serialize, Deserialize, Clone, Copy, PartialEq, Eq, Hash, ToSchema)]
+#[sqlx(transparent)]
+pub struct ChatId(i32);
+
+impl ChatId {
+    pub fn new(id: i32) -> Self {
+        Self(id)
+    }
+}
+
+impl From<i32> for ChatId {
+    fn from(value: i32) -> Self {
+        Self(value)
+    }
+}
+
+impl std::fmt::Display for ChatId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl PartialEq<i32> for ChatId {
+    fn eq(&self, other: &i32) -> bool {
+        self.0 == *other
+    }
+}
+
+impl Deref for ChatId {
+    type Target = i32;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
