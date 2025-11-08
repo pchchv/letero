59d80120812f5ef378aabf2fdd53449a00fe542c
pchchv <ipchchv@gmail.com>
2025-11-08 03:45:05 +0300

    feat(services): implement unauthorized function

--- server/src/services/auth.rs
+++ server/src/services/auth.rs
@@ -1,8 +1,17 @@
-use crate::models::users::User;
+use axum::response::Response;
+use crate::{
+    models::users::User,
+    services::trace::TraceId,
+    error::ApiError,
+};
 
 pub const SESSION_COOKIE_NAME: &str = "session";
 
 pub struct Auth {
     pub session: String,
     pub user: User,
+}
+
+fn unauthorized(trace_id: TraceId) -> Response {
+    ApiError::Unauthorized { trace_id }.into_response()
 }
