41c0042b897feb824a3db16700c9639b304a74fb
pchchv <ipchchv@gmail.com>
2025-11-08 04:05:45 +0300

    refactor(repositories): update create_user func

--- server/src/repositories/users.rs
+++ server/src/repositories/users.rs
@@ -1,9 +1,9 @@
-use crate::error::RepositoryError;
+use crate::{error::RepositoryError, models::users::{PasswordHash, UserId}};
 
 #[cfg_attr(test, mockall::automock)]
 #[async_trait::async_trait]
 pub trait UsersRepository: Send + Sync {
-    async fn create_user(&self, username: &str, password: &str) -> Result<i32, RepositoryError>;
+    async fn create_user(&self, username: &str, password: PasswordHash) -> Result<UserId, RepositoryError>;
 }
 
 pub struct PgUsersRepository(sqlx::PgPool);
@@ -16,15 +16,16 @@ impl PgUsersRepository {
 
 #[async_trait::async_trait]
 impl UsersRepository for PgUsersRepository {
-    async fn create_user(&self, username: &str, password: &str) -> Result<i32, RepositoryError> {
-        let result = sqlx::query!(
-            "INSERT INTO Users (Name, Password) VALUES ($1, $2) RETURNING Id",
+    async fn create_user(&self, username: &str, password: PasswordHash) -> Result<UserId, RepositoryError> {
+        let result = sqlx::query_scalar!(
+            "INSERT INTO Users (Name, Password, Salt) VALUES ($1, $2, $3) RETURNING Id",
             username,
-            password
+            *password,
+            password.get_salt(),
         )
         .fetch_one(&self.0)
         .await?;
 
-        Ok(result.id)
+        Ok(UserId::new(result))
     }
 }
