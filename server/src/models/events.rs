8e75984bb2a08fded031b4bfcccc0657d8088b1d
pchchv <ipchchv@gmail.com>
2025-11-09 18:58:19 +0300

    feat(models): implement SseEvent

--- server/src/models/events.rs
+++ server/src/models/events.rs
@@ -5,3 +5,16 @@ pub enum SseEventType {
     Message,
     Chat,
 }
+
+#[derive(Clone)]
+pub struct SseEvent {
+    pub event_type: SseEventType,
+    pub data: String,
+}
+
+impl SseEvent {
+    pub fn new(event_type: SseEventType, data: impl serde::Serialize) -> Self {
+        let data = serde_json::to_string(&data).expect("failed to serialize event data");
+        Self { event_type, data }
+    }
+}
