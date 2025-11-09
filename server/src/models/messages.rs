347a483b39cd820a39ab97a79ea91ee5ad0571ce
pchchv <ipchchv@gmail.com>
2025-11-09 19:21:12 +0300

    feat(models): implement a Message

--- server/src/models/messages.rs
+++ server/src/models/messages.rs
@@ -1,6 +1,17 @@
+use crate::models::{chats::ChatId, users::UserId};
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use std::ops::Deref;
+
+#[derive(Serialize, Debug, ToSchema, Clone)]
+pub struct Message {
+    pub id: MessageId,
+    pub content: String,
+    pub chat_id: ChatId,
+    pub sender_id: Option<UserId>,
+    #[serde(with = "time::serde::iso8601")]
+    pub created_at: time::OffsetDateTime,
+}
 
 #[derive(Debug, Clone, Copy, Deserialize, Serialize, sqlx::Type, PartialEq, ToSchema)]
 #[sqlx(transparent)]
