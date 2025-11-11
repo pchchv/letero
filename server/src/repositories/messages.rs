3e435d72a8180cd551bb14ec317caf9b494cfa85
pchchv <ipchchv@gmail.com>
2025-11-11 20:05:10 +0300

    feat(repos): implement MessagesRepository

---
+++ server/src/repositories/messages.rs
@@ -0,0 +1,26 @@
+use crate::{
+    error::RepositoryError,
+    models::{
+        chats::ChatId,
+        messages::{Message, MessageId},
+        users::UserId,
+    },
+};
+
+#[cfg_attr(test, mockall::automock)]
+#[async_trait::async_trait]
+pub trait MessagesRepository: Send + Sync {
+    async fn get_messages(
+        &self,
+        chat_id: ChatId,
+        limit: i64,
+        last_message_id: Option<MessageId>,
+    ) -> Result<Vec<Message>, RepositoryError>;
+
+    async fn create_message(
+        &self,
+        chat_id: ChatId,
+        user_id: UserId,
+        content: &str,
+    ) -> Result<Message, RepositoryError>;
+}
