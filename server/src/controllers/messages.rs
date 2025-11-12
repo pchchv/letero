468c00101ef6435621f094e6a51471c78e897a48
pchchv <ipchchv@gmail.com>
2025-11-12 21:11:42 +0300

    feat(conrollers): implement new_message func

--- server/src/controllers/messages.rs
+++ server/src/controllers/messages.rs
@@ -1,7 +1,106 @@
+use axum::{Extension, Json, extract::{Path, State}};
+use std::{collections::HashMap, sync::Arc};
 use crate::{
+    AppState,
+    error::ApiError,
     repositories::chats::ChatsRepository,
-    models::{chats::ChatId, users::UserId},
+    services::{auth::Auth, trace::TraceId},
+    models::{
+        chats::ChatId,
+        users::UserId,
+        events::{MessageEvent, SseEvent, SseEventType},
+        messages::{NewMessageRequest, NewMessageResponse, MessageId},
+    },
 };
+
+/// Send message to chat
+#[utoipa::path(
+    post,
+    path = "/chats/{chat_id}",
+    tag = "messages",
+    params(
+        ("chat_id" = ChatId, Path, description = "Chat id")
+    ),
+    request_body = NewMessageRequest,
+    responses(
+        (status = OK, description = "Message sent", body = NewMessageResponse, example = json!(NewMessageResponse { message_id: MessageId::from(84735) })),
+        (status = BAD_REQUEST, description = "Most likely, you have specified empty content", example = json!({"type": "ValidationError", "trace_id": TraceId::new()})),
+        (status = FORBIDDEN, description = "Forbidden", example = json!({"type": "Forbidden", "trace_id": TraceId::new()})),
+        (status = INTERNAL_SERVER_ERROR, description = "Internal server error", example = json!({"type": "Unknown", "trace_id": TraceId::new()}))
+    ),
+    security(("auth" = []))
+)]
+pub async fn new_message(
+    Extension(auth): Extension<Arc<Auth>>,
+    Extension(trace_id): Extension<TraceId>,
+    State(state): State<Arc<AppState>>,
+    Path(chat_id): Path<ChatId>,
+    Json(req): Json<NewMessageRequest>,
+) -> Result<NewMessageResponse, ApiError> {
+    tracing::trace!("new message for chat {chat_id} from user {}", auth.user.id);
+
+    if !check_chat_access(&*state.chats, auth.user.id, chat_id).await {
+        tracing::error!("forbidden");
+        return Err(ApiError::Forbidden { trace_id });
+    }
+
+    let mut errors = HashMap::new();
+    let content_errors = req.content.validate();
+
+    if !content_errors.is_empty() {
+        errors.insert("content".to_string(), content_errors);
+    }
+
+    if !errors.is_empty() {
+        return Err(ApiError::Validation {
+            trace_id,
+            fields: errors,
+        });
+    }
+
+    let message = match state
+        .messages
+        .create_message(chat_id, auth.user.id, req.content.as_ref())
+        .await
+    {
+        Ok(message) => {
+            tracing::trace!("message created with id {}", *message.id);
+            message
+        }
+        Err(e) => {
+            tracing::error!("failed to create message: {e}");
+            return Err(ApiError::Unknown { trace_id });
+        }
+    };
+
+    let chat_members = state.chats.get_chat_members(chat_id).await.map_err(|e| {
+        tracing::error!("failed to get chat members: {e}");
+        ApiError::Unknown { trace_id }
+    })?;
+
+    for member in chat_members {
+        if member == auth.user.id {
+            continue;
+        }
+
+        if let Some(member) = state.events.get(&member) {
+            if let Err(e) = member.send(SseEvent::new(
+                SseEventType::Message,
+                MessageEvent {
+                    user_id: auth.user.id,
+                    message: message.clone(),
+                    chat_id,
+                },
+            )) {
+                tracing::error!("failed to send message event: {e}");
+            }
+        }
+    }
+
+    Ok(NewMessageResponse {
+        message_id: message.id,
+    })
+}
 
 #[tracing::instrument(skip(chats), ret)]
 async fn check_chat_access(chats: &dyn ChatsRepository, user_id: UserId, chat_id: ChatId) -> bool {
