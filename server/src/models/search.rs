ab10ad4d9d589aac6454c72919933bf24cac0b2a
pchchv <ipchchv@gmail.com>
2025-11-13 20:50:14 +0300

    feat(models): implement a SearchUsersQuery

--- server/src/models/search.rs
+++ server/src/models/search.rs
@@ -1,6 +1,6 @@
-use crate::models::users::PublicUser;
+use crate::models::users::{PublicUser, Username};
+use serde::{Deserialize, Serialize};
 use axum::response::IntoResponse;
-use serde::Serialize;
 use utoipa::ToSchema;
 
 #[derive(Serialize, ToSchema)]
@@ -10,4 +10,9 @@ impl IntoResponse for SearchUsersResponse {
     fn into_response(self) -> axum::response::Response {
         axum::Json(self).into_response()
     }
+}
+
+#[derive(Deserialize, ToSchema)]
+pub struct SearchUsersQuery {
+    pub username: Username,
 }
